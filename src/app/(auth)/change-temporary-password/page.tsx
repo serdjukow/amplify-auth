914f16b5487e95f68b5a8beeab9fb0b94606ba61
serdjukow <serduykovs@yandex.ru>
2025-03-28 17:29:01 +0100

    Fix

--- src/app/(auth)/change-temporary-password/page.tsx
+++ src/app/(auth)/change-temporary-password/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import ChangeTemporaryPassword from "@/components/auth/ChangeTemporaryPassword"
 
 const ChangeTemporaryPasswordPage = () => {
--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import ForgotPassword from "@/components/auth/ForgotPassword"
 
 const ForgotPasswordPage = () => {
